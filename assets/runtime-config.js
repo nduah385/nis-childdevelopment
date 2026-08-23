7cc90333a40cea89119f588725aa203c478cb358
Ephraim Owusu-Duah <nduah385@gmail.com>
2026-08-23 19:39:58 +0000

    Normalize v2.2.2 Neon runtime URL

--- assets/runtime-config.js
+++ assets/runtime-config.js
@@ -1,5 +1,5 @@
 window.NIS_CONFIG = {
-  neonAuthUrl: "https://ep-quiet-star-af3t7s87.neonauth.c-2.us-west-2.aws/neondb/auth".replace("aws/","aws.neon.tech/"),
+  neonAuthUrl: "https://ep-quiet-star-af3t7s87.neonauth.c-2.us-west-2.aws.neon.tech/neondb/auth",
   neonDataApiUrl: "https://ep-quiet-star-af3t7s87.apirest.c-2.us-west-2.aws.neon.tech/neondb/rest/v1",
   canonicalUrl: "https://nduah385.github.io/nis-childdevelopment/",
   version: "2.2.2-neon-r2",
