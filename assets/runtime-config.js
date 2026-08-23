1255c155526ae364ab69cfb8a2b9f343e028cd50
Ephraim Owusu-Duah <nduah385@gmail.com>
2026-08-23 14:14:40 +0000

    Add files via upload

---
+++ assets/runtime-config.js
@@ -0,0 +1,6 @@
+window.NIS_CONFIG = {
+  neonAuthUrl: "https://ep-quiet-star-af3t7s87.neonauth.c-2.us-west-2.aws.neon.tech/neondb/auth",
+  neonDataApiUrl: "https://ep-quiet-star-af3t7s87.apirest.c-2.us-west-2.aws.neon.tech/neondb/rest/v1",
+  canonicalUrl: "https://nduah385.github.io/nis-childdevelopment/",
+  version: "2.0.0-neon"
+};
